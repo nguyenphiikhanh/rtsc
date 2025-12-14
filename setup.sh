71727445bc07a98ddad172102ce5e941fb6e2e7d
khanhnt <nguyenphiikhanh@gmail.com>
2025-12-15 01:24:46 +0700

    feat: setup config file

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
- cp admin/config.php.example admin/config.php
+#!/bin/bash
 
- cp config/config.php.example config/config.php
+cp admin/config.php.example admin/config.php
+cp config/config.php.example config/config.php
+
+echo "Done copying config files."
