0c74ecd448012e76f64465ccaaeaceabaacf1b99
khanhnt <nguyenphiikhanh@gmail.com>
2025-12-18 18:45:55 +0700

    audit fix: no content response

--- api/audit.php
+++ api/audit.php
@@ -1,9 +1,6 @@
 <?php
 require_once __DIR__ . '/../config/config.php';
 global $config;
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 
 /* ===== CORS ===== */
 header("Access-Control-Allow-Origin: *");
