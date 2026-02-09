39c1174aa765a63dbccddcf5fadd3155cfa292c0
khanhnt <nguyenphiikhanh@gmail.com>
2026-02-09 23:59:06 +0700

    fix: giftcode

--- modules/giftcode.php
+++ modules/giftcode.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/../config/config.php';
 function __get_gift_codes()
 {
     global $config;
-    $query = "SELECT * FROM gift_codes WHERE gift_codes.active = 0";
+    $query = "SELECT * FROM giftcode";
 
     $result = $config->query($query);
     $data = [];
